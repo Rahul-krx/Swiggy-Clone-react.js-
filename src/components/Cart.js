7a811f3099fbba975f462bc1106141bd92c8b69f
Rahul Kumar <krahul86946@gmail.com>
2025-10-13 23:17:34 +0530

    implemented clear from cart feature

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,22 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () =>{
-    const cartItems = useSelector((store) => store.Cart.items);
+    const cartItems = useSelector((store) => store.Cart?.items || []);
+
+    const dispatch = useDispatch();
+const handleClearcart = () =>{
+    dispatch(clearCart)
+
+}
+
     return(
         <div>
-          <h1 className="text-center text-2xl font-bolds">Cart</h1>
+          <h1 className="text-center text-2xl font-bold">Cart</h1>
           <div>
+            <button className="p-2 m-3 rounded-lg bg-blue-500 " onClick={handleClearcart} >Clear Cart</button>
+            {cartItems.length === 0 && <h2 className="text-center -mt-6 text-xl font-semibold">Your cart is empty!🥲</h2>}
             <ItemList items={cartItems}/>
           </div>
         </div>
--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -13,12 +13,12 @@ const cartSlice = createSlice({
             state.items.pop();
 
         },
-        clearItem: (state) =>{
+        clearCart: (state) =>{
             state.items.length = 0;
         },
     },
 });
 
-export const {addItem, removeItem, clearItem}= cartSlice.actions;
+export const {addItem, removeItem, clearCart}= cartSlice.actions;
 
 export default cartSlice.reducer;
